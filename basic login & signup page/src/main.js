ecae74ffe239959df5eb589be34dfd55749e4aeb
Atharva Vibhute <94898067+atharvavibe@users.noreply.github.com>
2022-07-23 16:30:57 +0530

    Add files via upload

---
+++ basic login & signup page/src/main.js
@@ -0,0 +1,36 @@
+function setFormMessage(formElement, type, message){
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const loginForm = document.querySelector('#login');
+    const createAccountForm = document.querySelector('#createAccount');
+
+    document.querySelector('#linkCreateAccount').addEventListener("click", e => {
+
+        e.preventDefault();
+        loginForm.classList.add('form__hidden');
+        createAccountForm.classList.remove('form__hidden');
+    });
+
+    document.querySelector('#linkLogin').addEventListener("click", e => {
+
+        e.preventDefault();
+        loginForm.classList.remove('form__hidden');
+        createAccountForm.classList.add('form__hidden');
+    });
+
+    loginForm.addEventListener("submit", e => {
+
+        e.preventDefault();
+        setFormMessage(loginForm, 'error', 'Invalid Username/ Password');
+    });
+});
